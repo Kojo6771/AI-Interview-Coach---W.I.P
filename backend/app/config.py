4fea56593beb2c83f2ebe5e3f887084a59698993
Kojo6771 <230165858@aston.ac.uk>
2026-08-08 04:52:03 +0100

    created hashing and updated config
    I have created the hashing file which will be used to do the password hashing. I also edited the config to include the secret key and algorithm which are in the .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 # Get the database URL from environment variables, with a safe fallback for local development
 Database_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+
+Secret_Key = os.getenv("SECRET_KEY")
+Algorithm = os.getenv("ALGORITHM")
+Access_Token_Expire_Minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
